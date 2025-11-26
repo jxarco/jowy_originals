a996927af2ee5da019f95224ac2cfe18bebaf1f4
jxarco <alexroco.30@gmail.com>
2025-11-26 21:16:37 +0100

    Local lexgui fixes

--- lexgui.module.js
+++ lexgui.module.js
@@ -457,16 +457,16 @@ async function init( options = { } )
     this.layoutMode = options.layoutMode ?? "app";
     document.documentElement.setAttribute( "data-layout", this.layoutMode );
 
-    if( this.layoutMode == "document" )
-    {
-        document.addEventListener( "scroll", e => {
-            // Get all active menuboxes
-            const mbs = document.body.querySelectorAll( ".lexdropdownmenu" );
-            mbs.forEach( ( mb ) => {
-                mb._updatePosition();
-            } );
-        } );
-    }
+    // if( this.layoutMode == "document" )
+    // {
+    //     document.addEventListener( "scroll", e => {
+    //         // Get all active menuboxes
+    //         const mbs = document.body.querySelectorAll( ".lexdropdownmenu" );
+    //         mbs.forEach( ( mb ) => {
+    //             mb._updatePosition();
+    //         } );
+    //     } );
+    // }
 
     this.spacingMode = options.spacingMode ?? "default";
     document.documentElement.setAttribute( "data-spacing", this.spacingMode );
@@ -1886,9 +1886,9 @@ class Calendar {
 
     _previousMonth( skipCallback ) {
 
-        this.month = Math.max( 0, this.month - 1 );
+        this.month = Math.max( 1, this.month - 1 );
 
-        if( this.month == 0 )
+        if( this.month == 1 )
         {
             this.month = 12;
             this.year--;
@@ -1904,11 +1904,11 @@ class Calendar {
 
     _nextMonth( skipCallback ) {
 
-        this.month = Math.min( this.month + 1, 12 );
+        this.month = Math.min( this.month + 1, 13 );
 
-        if( this.month == 12 )
+        if( this.month == 13 )
         {
-            this.month = 0;
+            this.month = 1;
             this.year++;
         }
 
@@ -2073,8 +2073,7 @@ class Calendar {
     fromMonthYear( month, year ) {
 
         // Month is 0-based (0 = January, ... 11 = December)
-        month--;
-
+        month = Math.max( month - 1, 0 );
         year = year ?? new Date().getFullYear();
 
         const weekDay = new Date( year, month, 1 ).getDay();
@@ -4786,6 +4785,34 @@ else
         return new Date().getTime();
     };
 }
+
+function stripTags( str, allowed )
+{
+    // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+    allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('')
+
+    const tags = /<\/?([a-z0-9]*)\b[^>]*>?/gi
+    const commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi
+
+    let after = str;
+    // removes tha '<' char at the end of the string to replicate PHP's behaviour
+    after = after.substring(after.length - 1) === '<' ? after.substring(0, after.length - 1) : after
+
+    // recursively remove tags to ensure that the returned string doesn't contain forbidden tags after previous passes (e.g. '<<bait/>switch/>')
+    while (true) {
+        const before = after
+        after = before.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+            return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
+        })
+
+        // return once no more tags are removed
+        if (before === after) {
+            return after
+        }
+    }
+}
+
+LX.stripTags = stripTags;
 
 /**
  * @method doAsync
@@ -12889,7 +12916,7 @@ class Table extends BaseComponent
                                     f.default = [ todayStringDate, todayStringDate ];
                                 }
 
-                                const calendar = new LX.CalendarRange( f.value, {
+                                const calendar = new LX.CalendarRange( f.value ?? f.default, {
                                     onChange: ( dateRange ) => {
                                         f.value = dateRange;
                                         spanName.innerHTML = icon.innerHTML + f.name + ( separatorHtml + LX.badge( `${ calendar.getFullDate() }`, "bg-tertiary fg-secondary text-sm border-0" ) );
@@ -12922,7 +12949,7 @@ class Table extends BaseComponent
                         }
                         else if( f.type == "date" )
                         {
-                            delete f.default;
+                            delete f.value;
                         }
                     }
                     this.refresh();
@@ -13205,8 +13232,9 @@ class Table extends BaseComponent
                             const filterColIndex = data.head.indexOf( acfName );
                             if( filterColIndex > -1 )
                             {
-                                debugger;
-                                acfMap[ acfName ] |= ( bodyData[ filterColIndex ] === acfValue );
+                                const cellValue = bodyData[ filterColIndex ];
+                                const strippedValue = LX.stripTags( cellValue ) ?? cellValue;
+                                acfMap[ acfName ] |= ( strippedValue === acfValue );
                             }
                         }
 
@@ -13246,11 +13274,8 @@ class Table extends BaseComponent
                                 const filterColIndex = data.head.indexOf( acfName );
                                 if( filterColIndex > -1 )
                                 {
-                                    if( !f.default )
+                                    if( !f.value )
                                     {
-                                        const date = new Date();
-                                        const todayStringDate = `${ date.getDate() }/${ date.getMonth() + 1 }/${ date.getFullYear() }`;
-                                        f.value = [ todayStringDate, todayStringDate ];
                                         acfMap[ acfName ] |= true;
                                         continue;
                                     }
