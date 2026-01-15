88dc3bd169edf88715a62a13032a6966354442cb
jxarco <alexroco.30@gmail.com>
2026-01-15 23:30:05 +0100

    map skus in orders app

--- src/apps/manual_app.js
+++ src/apps/manual_app.js
@@ -49,15 +49,22 @@ class ManualApp
 
             docMaker.lineBreak();
 
-            docMaker.header( 'Shader Passes.', 'h2', 'shader-passes' );
+            docMaker.header( 'Acciones.', 'h2' );
+            docMaker.lineBreak();
 
             docMaker.paragraph(
-                `ShaderHub supports multiple shader passes, which are essentially different stages of rendering that can be combined to create complex visual effects. There are 3 types of passes you can create:`
+                `Acciones principales:`
             );
             docMaker.bulletList( [
-                `<span class='font-bold underline underline-offset-4'>Buffers</span>: Offscreen render targets that can be used to store intermediate results. You can create up to four buffer passes, which can be referenced in subsequent passes using the iChannel uniforms (iChannel0, iChannel1, etc.). This allows you to build effects step by step, using the output of one pass as the input for another.`,
-                `<span class='font-bold underline underline-offset-4'>Compute</span>: A compute pass that runs independently of the render pipeline and writes directly to buffers or textures. This is useful for general-purpose GPU computations such as simulations, particle updates, procedural data generation, or precomputing values that will later be used by render or buffer passes.`,
-                `<span class='font-bold underline underline-offset-4'>Common</span>: Used for shared code that can be included in other passes. This is useful for defining functions or variables that you want to reuse across multiple shader passes. You can only have one Common pass per shader.`
+                `<span class='font-bold'>Ver mensajes</span>: ...`,
+                `<span class='font-bold'>Limpiar datos anteriores</span>: ...`,
+                `<span class='font-bold'>Nuevo pedido</span>: ...`,
+                `<span class='font-bold'>Más opciones</span>: ...`,
+                [
+                    `<span class=''>Abrir pedidos</span>: ...`,
+                    `<span class=''>Marcar seleccionados</span>: ...`,
+                    `<span class=''>Actualizar pedidos</span>: ...`,
+                ]
             ] );
         }
 
