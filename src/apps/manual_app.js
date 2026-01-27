8c3096bf2fa1b2de94a2d7051afa012d3a18ae01
jxarco <alexroco.30@gmail.com>
2026-01-27 01:54:30 +0100

    Start SHEIN/Tiktok app docs

--- src/apps/manual_app.js
+++ src/apps/manual_app.js
@@ -30,8 +30,20 @@ class ManualApp extends BaseApp
 
         {
             const sheinContainer = LX.makeContainer( [ null, 'auto' ], 'flex flex-col relative bg-card p-1 pt-0 rounded-lg overflow-hidden' );
-            tabs.add( 'Shein', sheinContainer, { xselected: true, onSelect: ( event, name ) => {} } );
-            sheinContainer.appendChild( this.createSheinDocs() );
+            tabs.add( 'SHEIN', sheinContainer, { selected: true, onSelect: ( event, name ) => {} } );
+            sheinContainer.appendChild( this.createSheinDocs( 'SHEIN', 'shein.com' ) );
+        }
+
+        {
+            const tiktokContainer = LX.makeContainer( [ null, 'auto' ], 'flex flex-col relative bg-card p-1 pt-0 rounded-lg overflow-hidden' );
+            tabs.add( 'TikTok', tiktokContainer, { xselected: true, onSelect: ( event, name ) => {} } );
+            tiktokContainer.appendChild( this.createSheinDocs( 'TikTok', 'tiktok.com' ) );
+        }
+
+        {
+            const miraviaContainer = LX.makeContainer( [ null, 'auto' ], 'flex flex-col relative bg-card p-1 pt-0 rounded-lg overflow-hidden' );
+            tabs.add( 'Miravia', miraviaContainer, { xselected: true, onSelect: ( event, name ) => {} } );
+            miraviaContainer.appendChild( this.createMiraviaDocs() );
         }
 
         {
@@ -40,11 +52,11 @@ class ManualApp extends BaseApp
             decathlonContainer.appendChild( this.createDecathlonDocs() );
         }
 
-        {
-            const transportCalcContainer = LX.makeContainer( [ null, 'auto' ], 'flex flex-col relative bg-card p-1 pt-0 rounded-lg overflow-hidden' );
-            tabs.add( 'Calc. Transporte', transportCalcContainer, { xselected: true, onSelect: ( event, name ) => {} } );
-            transportCalcContainer.appendChild( this.createTransportCalcDocs() );
-        }
+        // {
+        //     const transportCalcContainer = LX.makeContainer( [ null, 'auto' ], 'flex flex-col relative bg-card p-1 pt-0 rounded-lg overflow-hidden' );
+        //     tabs.add( 'Calc. Transporte', transportCalcContainer, { xselected: true, onSelect: ( event, name ) => {} } );
+        //     transportCalcContainer.appendChild( this.createTransportCalcDocs() );
+        // }
 
         this.clear();
     }
@@ -134,9 +146,16 @@ class ManualApp extends BaseApp
                 docMaker.paragraph( `En la parte superior de la aplicación se encuentra el área de carga de archivos, donde se puede arrastrar el archivo XLSX generado desde CBL (o hacer click para buscarlo) para cargar el listado de envíos. Al realizar la carga, la aplicación leerá el listado de envíos y relacionará cada una de las filas con los pedidos existentes en WooCommerce, <b>sincronizando únicamente aquellos pedidos cuyo número de pedido esté incluido en el campo de referencia</b>.` );
                 docMaker.paragraph( `Si un pedido no dispone de número de referencia, <b>no podrá ser leído ni sincronizado por la aplicación</b>. En listados con más de 100 pedidos, el proceso puede comenzar a funcionar de forma más lenta, lo cual es un comportamiento normal del sistema.` );
             } } );
-
+            
+            docMaker.header( 'Selección por empresa.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
+                docMaker.paragraph( `En la parte superior de la aplicación justo después del encabezado se encuentra el selector de compañía, dividido en pestañas (Jowy, HxG, Bathby, y Otros), donde cada una de ellas muestra los pedidos correspondientes a su plataforma.
+                    En la pestaña <b>Otros</b> no se realiza ninguna relación ni sincronización; los pedidos se muestran únicamente en forma de lista informativa, como por ejemplo los pedidos de Amazon.` );
+                docMaker.image( 'data/docs/company_tabs.png', 'company_tabs', null, 'my-4 w-[90%] md:w-[400px] place-self-center' );
+                docMaker.paragraph( `Al hacer click en una de las pestañas, la aplicación automáticamente carga los pedidos de dicha compañía e inicia la sincronización con el listado de CBL. <b>Recuerda que en ese instante es cuando se requerirán las claves de acceso</b>.` );
+            } } );
+            
             docMaker.header( 'Acciones principales.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
-                docMaker.paragraph( `En la parte superior de la aplicación, justo después del encabezado, se encuentran las acciones principales representadas mediante iconos:` );
+                docMaker.paragraph( `A la derecha de las pestañas de cada compañia se encuentran las acciones principales representadas mediante iconos:` );
 
                 docMaker.header( LX.makeIcon( 'Eye', { svgClass: 'border-color rounded-lg p-2 mr-2 w-10! h-10! inline-flex!' } ).innerHTML + 'Ver mensajes.', 'h3' );
                 docMaker.paragraph( `Permite visualizar los pedidos de forma individual. En esta vista se muestra la información completa del pedido junto con el formulario de facturación de WooCommerce. En dicho formulario es posible editar <b>únicamente el número de factura y la fecha de factura<b>.
@@ -171,13 +190,6 @@ class ManualApp extends BaseApp
                 );
             } } );
 
-            docMaker.header( 'Selección por empresa.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
-                docMaker.paragraph( `En la misma fila que las acciones principales se encuentra el selector de compañía, dividido en pestañas (Jowy, HxG, Bathby, y Otros), donde cada una de ellas muestra los pedidos correspondientes a su plataforma.
-                    En la pestaña <b>Otros</b> no se realiza ninguna relación ni sincronización; los pedidos se muestran únicamente en forma de lista informativa, como por ejemplo los pedidos de Amazon.` );
-                docMaker.image( 'data/docs/company_tabs.png', 'company_tabs', null, 'my-4 w-[90%] md:w-[400px] place-self-center' );
-                docMaker.paragraph( `Al hacer click en una de las pestañas, la aplicación automáticamente carga los pedidos de dicha compañía e inicia la sincronización con el listado de CBL. <b>Recuerda que en ese instante es cuando se requerirán las claves de acceso</b>.` );
-            } } );
-
             docMaker.header( 'Tabla de pedidos.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
                 docMaker.paragraph( `El área central de la aplicación contiene una tabla donde cada fila representa un pedido, donde se pueden ocultar o mostrar columnas, y la cual puede filtrar por:` );
                 docMaker.bulletList(
@@ -202,14 +214,108 @@ class ManualApp extends BaseApp
         return area.root;
     }
 
-    createSheinDocs()
+    createSheinDocs( appName, appMarketplaceUrl )
     {
         const area = new LX.Area( { className: 'help-content rounded-lg bg-card p-8' } );
 
         const docMaker = new DocMaker();
         docMaker.setDomTarget( area.root );
 
-        docMaker.header( LX.makeIcon( 'Construction', { svgClass: 'mr-4 2xl inline-flex!' } ).innerHTML + 'Shein.', 'h1' );
+        docMaker.header( `${ appName }.`, 'h1' );
+        {
+            docMaker.paragraph( `La aplicación de ${ appName } agrupa todos los procesos operativos necesarios para la gestión diaria y semanal de pedidos,
+                incluyendo el control de stock, la generación de etiquetas de transporte SEUR, la incorporación de información de seguimiento y la creación de la
+                documentación fiscal (IVA) y de albaranes, los cuales serán posteriormente traspasados a Factusol.
+                <b>Todos los procesos se realizan a partir de archivos Excel generados por plataformas externas y por las propias aplicaciones internas</b>.` );
+            docMaker.lineBreak();
+            docMaker.paragraph( `Para completar todos los procesos, necesitarás acceso al Marketplace de ${appName}
+                (<a class='bg-red-600 text-primary underline underline-offset-4' href=''>${appMarketplaceUrl}</a>)
+                y al cliente web de SEUR (<a class='bg-red-600 text-primary underline underline-offset-4' href=''>seur.com</a>).` );
+
+            docMaker.header( 'Carga del listado de envíos.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
+                
+                docMaker.paragraph( `El primer paso es obtener un archivo <span class='font-code'>.xlsx</span> con la información de los pedidos.
+                    Para ello, accede al marketplace, usando el enlace <a class='bg-red-600 text-primary underline underline-offset-4' href=''>${appMarketplaceUrl}</a>.
+                    <span class='bg-red-600'>Una vez dentro, ...</span>` );
+
+                docMaker.paragraph( `Obtenido el listado de pedidos, dirígete a la parte superior de la aplicación donde se encuentra el área de carga de archivos, y de nuevo
+                    arrastra allí el archivo exportado. Estos datos actuarán como base para los siguientes procesos dentro de la aplicación.` );
+            } } );
+
+            docMaker.header( 'Ver datos importados.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
+                docMaker.paragraph( `En la parte superior de la aplicación justo después del encabezado se encuentra el selector de datos, dividido en pestañas, donde se puede
+                    ver la información importada y los pasos intermedios del proceso. Revisa estos datos siempre que puedas para comprobar que no haya ningún error.` );
+                docMaker.image( 'data/docs/company_tabs.png', 'company_tabs', null, 'my-4 w-[90%] md:w-[400px] place-self-center' );
+                docMaker.paragraph( `<span class='bg-red-600'>....</span>` );
+            } } );
+
+            docMaker.header( 'Listado stock.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
+                docMaker.paragraph( `Pulsando <b>Listado stock</b> en el selector de datos, tendrás acceso a una tabla con la información preparada para ser transferida
+                    al excel interno de <b>control de stock</b>. Actualiza ese archivo siguiendo estos pasos:` );
+                docMaker.bulletList(
+                    [
+                        `<span class='bg-red-600'>En Google Drive, abre el archivo <span class='font-code'>CONTROL STOCK [AÑO].xls</span>, ubicado en la ruta <span class='font-code'>RUTA/1/2/3</span>`,
+                        `<span class='bg-red-600'>Copia cada una de las columnas de la tabla, y pégalas en su correspodiente columna del excel de stock, en la pestaña <span class='font-code'>PESTAÑA</span></span>`,
+                        `<span class='bg-red-600'>...</span>`,
+                    ]
+                );
+            } } );
+
+            docMaker.header( 'Etiquetas y seguimiento.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
+                docMaker.paragraph( `En la parte superior de la aplicación, justo después del encabezado, se encuentran las acciones principales relacionadas con las etiquetas de SEUR, representadas mediante iconos:` );
+
+                docMaker.header( LX.makeIcon( 'Trash2', { svgClass: 'border-color rounded-lg p-2 mr-2 w-10! h-10! inline-flex!' } ).innerHTML + 'Limpiar datos anteriores.', 'h3' );
+                docMaker.paragraph( `Elimina completamente los datos cargados en la aplicación y limpia la tabla, permitiendo cargar un nuevo listado <span class='font-code'>.xlsx</span> e iniciar un nuevo proceso desde cero.` );
+
+                docMaker.header( LX.makeIcon( 'Download', { svgClass: 'border-color rounded-lg p-2 mr-2 w-10! h-10! inline-flex!' } ).innerHTML + 'Exportar Etiquetas.', 'h3' );
+                docMaker.paragraph( `A partir del excel de pedidos importado, descarga el fichero <span class='font-code'>ETIQUETAS_SEUR_${appName}_[FECHA].xlsx</span>, que tendrá los datos necesarios para generar etiquetas en la web de SEUR.
+                    Una vez exportado, accede a <a class='bg-red-600 text-primary underline underline-offset-4' href=''>seur.com</a> y sigue estos pasos para poder generarlas:` );
+                docMaker.bulletList(
+                    [
+                        `<span class='bg-red-600'>Abrir....</span>`,
+                        `<span class='bg-red-600'>Hacer click en....</span>`,
+                    ]
+                );
+
+                docMaker.header( LX.makeIcon( 'FileDown', { svgClass: 'border-color rounded-lg p-2 mr-2 w-10! h-10! inline-flex!' } ).innerHTML + 'Exportar Seguimiento.', 'h3' );
+                
+                docMaker.paragraph( `Para exportar los números de siguimiento de SEUR para asignar a los pedidos de ${appName}, necesitarás haber generado previamente las etiquetas y descargar de la web de SEUR un documento <span class='font-code'>.xlsx</span>
+                    con la información de las mismas, el cual contendrá el número de seguimiento de cada pedido. Para exportar dicho documento, <span class='bg-red-600'>abre....</span>` );
+                docMaker.lineBreak();
+                docMaker.paragraph( `Una vez dispongas de la lista de números de seguimiento, haz click en <b>Seguimiento</b> en el selector de datos y arrastra la lista exportada en el contenedor que aparecerá justo debajo
+                    para ver la información sincronizada con el listado de pedidos importado en el primer paso. Si todo está correcto, descarga el fichero <span class='font-code'>NUMERODEGUIA_SEUR_${appName}_[FECHA].xlsx</span> con los datos necesarios para asignar números de seguimiento a cada pedido dentro de ${appName}.
+                    Para hacerlo, usa el enlace <a class='bg-red-600 text-primary underline underline-offset-4' href=''>${appMarketplaceUrl}</a> de nuevo y sigue estos pasos:` );
+                
+                docMaker.bulletList(
+                    [
+                        `<span class='bg-red-600'>Abrir....</span>`,
+                        `<span class='bg-red-600'>Hacer click en....</span>`,
+                    ]
+                );
+            } } );
+
+            docMaker.header( 'IVA/Albaranes.', 'h2', area.root, { className: collapsibleClass, collapsable: true, collapsed: true, collapsableContentCallback: () => {
+                docMaker.paragraph( `La pestaña de IVA y albaranes concentra toda la información fiscal asociada a los pedidos.
+                    En esta vista se muestran los datos de <b>IVA</b>, el listado <b>LAL</b> con el detalle agregado de artículos y el listado <b>ALB</b> con la información por albarán.
+                    En esta sección es posible descargar el <b>documento de IVA generado<b> y un <b>archivo ZIP que contiene todos los albaranes organizados por país</b>.
+                    Para generar correctamente esta información, es necesario arrastrar uno o varios archivos <span class='font-code'>.xlsx</span> de pedidos en la zona de carga habilitada dentro de esta pestaña,
+                    permitiendo combinar información diaria, semanal o mensual según sea necesario.` );
+                docMaker.lineBreak();
+                docMaker.paragraph( `<span class='bg-red-600'>...</span>` );
+            } } );
+        }
+
+        return area.root;
+    }
+
+    createMiraviaDocs()
+    {
+        const area = new LX.Area( { className: 'help-content rounded-lg bg-card p-8' } );
+
+        const docMaker = new DocMaker();
+        docMaker.setDomTarget( area.root );
+
+        docMaker.header( LX.makeIcon( 'Construction', { svgClass: 'mr-4 2xl inline-flex!' } ).innerHTML + 'Miravia.', 'h1' );
 
         return area.root;
     }
